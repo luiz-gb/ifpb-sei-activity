f0d7c99c43c55528f48636bab8dc5ae9dabc08a5
unknown <luis.vitor@local>
2024-12-09 10:22:45 -0300

    retirando outros scripts

--- python/script.py
+++
@@ -1,9 +0,0 @@
-# Leia os valores de entrada
-a = int(input())
-b = int(input())
-
-# Calcule a soma
-x = a + b
-
-# Imprima o resultado
-print(f"X = {x}")
--- ruby/script.rb
+++
@@ -1,9 +0,0 @@
-# Leia os valores de entrada
-a = gets.to_i
-b = gets.to_i
-
-# Calcule a soma
-x = a + b
-
-# Imprima o resultado
-puts "X = #{x}"
